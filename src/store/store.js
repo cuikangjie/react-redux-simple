f802f7232749ecd4d842d4a9b4578cdbe76e7fc9
kin <cuikangjie_90h@126.com>
2017-12-14 13:54:52 +0800

    redux-thunk

--- src/store/store.js
+++ src/store/store.js
@@ -1,21 +1,13 @@
-import { createStore } from 'redux';
-import rootReducer from  '../reducers/index';
+import {createStore, applyMiddleware, compose} from "redux";
+import rootReducer from "../reducers/index";
+import thunkMiddleware from "redux-thunk";
+const __DEV__ = process.env.REACT_APP_DEV_ === "true";
 
-const __DEV__ = process.env.REACT_APP_DEV_ === "true"
-
-export default(initialState) => {
-
-  if(__DEV__){
-    return createStore(
-      rootReducer,
-      initialState,
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    );
-  }else{
-    return createStore(
-      rootReducer,
-      initialState
-    );
+export default initialState => {
+  if (__DEV__) {
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    return createStore(rootReducer, initialState, composeEnhancers(applyMiddleware(thunkMiddleware)));
+  } else {
+    return createStore(rootReducer, initialState, applyMiddleware(thunkMiddleware));
   }
-
-}
+};
